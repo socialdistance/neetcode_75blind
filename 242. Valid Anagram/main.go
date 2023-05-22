f9133813cdc2899e10f99ded3d7822ac96e77cee
socialdistance <pleaseuseurbrain@protonmail.com>
2023-05-22 17:36:02 +0300

    m

--- 242. Valid Anagram/main.go
+++ 242. Valid Anagram/main.go
@@ -17,12 +17,29 @@ import "fmt"
 // Output: false
 
 func isAnagram(s string, t string) bool {
+	mapResult := make(map[byte]int)
+
+	if len(s) != len(t) {
+		return false
+	}
+
+	for idx := 0; idx < len(s); idx++ {
+		mapResult[s[idx]]++
+		mapResult[t[idx]]--
+	}
+
+	for _, v := range mapResult {
+		if v != 0 {
+			return false
+		}
+	}
 
 	return true
 }
 
 func main() {
-	fmt.Println(isAnagram("anagram", "nagaram"))
-	fmt.Println(isAnagram("rat", "car"))
-	fmt.Println(isAnagram("aacc", "ccac"))
+	fmt.Println(isAnagram("a", "ab"))            // false
+	fmt.Println(isAnagram("anagram", "nagaram")) // true
+	fmt.Println(isAnagram("rat", "car"))         // false
+	fmt.Println(isAnagram("aacc", "ccac"))       // false
 }
