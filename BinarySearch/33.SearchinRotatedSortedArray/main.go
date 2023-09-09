21123aa2458661bdd909a54a206f703038911c99
socialdistance <pleaseuseurbrain@protonmail.com>
2023-09-09 20:11:42 +0300

    f

--- BinarySearch/33.SearchinRotatedSortedArray/main.go
+++ BinarySearch/33.SearchinRotatedSortedArray/main.go
@@ -26,11 +26,37 @@ import "fmt"
 // Output: -1
 
 func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
 
-	return 0
+	for l <= r {
+		mid := (l + r) / 2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		// проверяем левую часть
+		if nums[l] <= nums[mid] {
+			if target > nums[mid] || target < nums[l] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+			// Проверяем правую часть
+		} else {
+			if target < nums[mid] || target > nums[r] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		}
+	}
+
+	return -1
 }
 
 func main() {
-	fmt.Println(search([]int{4, 5, 6, 7, 0, 1, 2}, 0)) // 4
+	// fmt.Println(search([]int{4, 5, 6, 7, 0, 1, 2}, 0)) // 4
+	fmt.Println(search([]int{3, 1}, 1)) // 4
 	// fmt.Println(search([]int{4, 5, 6, 7, 0, 1, 2}, 3)) // -1
 }
